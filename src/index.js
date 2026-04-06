d69423b774f24e3a7c1d182ebea0a1353d438730
LyraMaker <gabriel.lyrat@gmail.com>
2026-04-06 16:12:25 -0300

    Refatorado o posicionamento das pastas e permitir que o site comporte como PWA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as bootstrap from 'bootstrap'
 import html2pdf from 'html2pdf.js'
 
-import * as Print from './print.js'
-import * as Data from './data.js'
-import alertModal from './alert.js'
+import * as Print from './public/js/print.js'
+import * as Data from './public/js/data.js'
+import alertModal from './public/js/alert.js'
 
 
 
@@ -35,3 +35,13 @@ btnSave.addEventListener('click', function (e) {
     worker.from(element).saveAs(filename)
   })
 })
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register(new URL("./sw.js", import.meta.url))
+      .then(() => console.log("Service Worker registrado"))
+      .catch(err => console.error("Erro:", err));
+  });
+}
